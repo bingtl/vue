da3dbb8e14ae82d45c770ba1198bedaca97ab13e
lpf <lpfa0402@163.com>
2019-06-20 16:54:16 +0800

    delete

--- testB/demoa/src/App.vue
+++ testB/demoa/src/App.vue
@@ -1,39 +1,18 @@
 <template>
-  <div class="wrapper">
-    <!-- 使用组件标签 -->
-    <div class="title">备忘录</div>
-    <Add/>
-    <List/>
+  <div id="app">
+    <div class="zj">
+       <p><router-link to="/testA">go to foo</router-link></p>
+      <p><router-link to="/testB">go to bar</router-link></p>
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script>
-// 引入组件
-import Add from './components/Add'
-import List from './components/List'
 export default {
-  // 将引入的组建映射成标签
-  components: {
-    Add,
-    List
-  }
 }
 </script>
 
 <style>
-.wrapper{
-    width: 1200px;
-    margin: 0 auto;
-    overflow: hidden;
-}
-.title{
-    width: 1200px;
-    height: 100px;
-    margin: 0 auto;
-    font-size: 40PX;
-    color: #383838;
-    font-weight: bold;
-    text-align: center;
-    line-height: 100PX;
-}
+
 </style>
--- testB/demoa/src/main.js
+++ testB/demoa/src/main.js
@@ -3,13 +3,14 @@
 */
 
 import Vue from 'vue'
+import router from './router'
 import App from './App.vue'
-
 // eslint-disable-next-line no-new
 new Vue({
   el: '#app',
   components: {
     App
   },
-  template: '<App/>'
+  template: '<App/>',
+  router: router
 })
